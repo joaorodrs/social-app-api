508efb19a25b99a266e7b8d957760b5853ae34ed
João Paulo <jpjoao1001@gmail.com>
2021-04-03 15:14:39 -0300

    created post controllers routes

--- src/post/post.entity.ts
+++ src/post/post.entity.ts
@@ -1,7 +1,28 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Post {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  ownerEmail: string;
+
+  @Column()
+  ownerId: string;
+
+  @Column()
+  postContent: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './post.entity';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(
+    @InjectRepository(Post)
+    private postRepository: Repository<Post>,
+  ) {}
+
+  async findRecents(): Promise<Post[]> {
+    return await this.postRepository.find({ order: { updatedAt: 'ASC' } });
+  }
+
+  async findAll(): Promise<Post[]> {
+    return await this.postRepository.find();
+  }
+
+  async findByOwnerId(ownerId: Post['ownerId']): Promise<Post[]> {
+    return await this.postRepository.find({ ownerId });
+  }
+
+  async findById(id): Promise<Post[]> {
+    return await this.postRepository.find(id);
+  }
+
+  async create(post: Post): Promise<Post> {
+    await this.postRepository.save(post);
+    return post;
+  }
+
+  async update(post: Post): Promise<Post> {
+    await this.postRepository.update(post.id, post);
+    return post;
+  }
+
+  async delete(id: Post['id']): Promise<any> {
+    return await this.postRepository.delete(id);
+  }
+}
