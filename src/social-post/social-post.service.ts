59dfec9dfc93dd147d32405d6bf6f04459a22e28
João Paulo <jpjoao1001@gmail.com>
2021-04-04 16:22:23 -0300

    added the services for social post

--- src/social-post/social-post.service.ts
+++ src/social-post/social-post.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { SocialPost } from './social-post.entity';
 
 @Injectable()
-export class SocialPostService {}
+export class SocialPostService {
+  constructor(private socialPostRepository: Repository<SocialPost>) {}
+
+  async create(socialPost: SocialPost): Promise<SocialPost> {
+    await this.socialPostRepository.save(socialPost);
+    return socialPost;
+  }
+
+  async findRecent(): Promise<SocialPost[]> {
+    return this.socialPostRepository.find({ order: { created_at: 'ASC' } });
+  }
+
+  async findByOwnerId(ownerId: SocialPost['owner_id']): Promise<SocialPost[]> {
+    return this.socialPostRepository.find({ owner_id: ownerId });
+  }
+
+  async findById(id: SocialPost['id']): Promise<SocialPost[]> {
+    return this.socialPostRepository.find({ where: { id } });
+  }
+
+  async update(socialPost: SocialPost): Promise<SocialPost> {
+    await this.socialPostRepository.update(socialPost.id, socialPost);
+    return socialPost;
+  }
+
+  async delete(id: SocialPost['id']): Promise<any> {
+    return await this.socialPostRepository.delete(id);
+  }
+}
