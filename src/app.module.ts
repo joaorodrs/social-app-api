4f8a3f570b0f48f8bb25f284d2aa4a7e5ac4cae1
João Paulo <jpjoao1001@gmail.com>
2021-04-17 17:45:29 -0300

    chore: config service file created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SocialPostModule } from './social-post/social-post.module';
+import { configService } from './config/config.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'social_app_db',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     SocialPostModule,
   ],
 })
